7d2bd5d9d5980de7c32eb869203c44948cdbd209
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-02-23 20:43:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,9 +3,15 @@ class Dog
       @name = name
     end
 
+def name=(name)
+  @name = name
+end
     def name
       @name
     end
+    def initialize(breed)
+        @breed = breed
+      end
 
     def breed=(breed)
       @breed = breed
