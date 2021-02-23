48af95c538cbe487d49268d4f32f1c4b506e83b5
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-02-23 20:46:05 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,7 @@
 class Person
-  def initialize(name)
-    @name = name
-  end
+  #def initialize(name)
+  #  @name = name
+  #end
 
   def name=(name)
     @name
@@ -11,9 +11,9 @@ class Person
     @name
   end
 
-  def initialize(job)
-    @job = job
-  end
+#  def initialize(job)
+#    @job = job
+#  end
 
   def job=(job)
     @job
